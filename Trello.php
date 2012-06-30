ee32f0d6baa9c708b4ba789e1993c645c33a959f
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-06-30 03:28:34 -0500

    First attempt to connect to Trello

---
+++ Trello.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Contains base class for PHP-Trello
+ */
+
+/**
+ * Class containing essential functions for interacting with the Trello API
+ */
+class Trello {
+
+  protected $username;
+  protected $apiKey;
+
+  /**
+   * @param string $user
+   *   The username for your Trello account
+   * @param string $apiKey
+   *   The API key provided by Trello
+   */
+  public function __construct($username, $apiKey) {
+    $this->username = $username;
+    $this->apiKey = $apiKey;
+  }
+
+  /**
+   * Build the URL for accessing Trello
+   *
+   * @param $path
+   *
+   * @return string
+   */
+  protected function apiUrl($path) {
+    $url = 'https://api.trello.com/1' . $path . '?key=' . $this->apiKey;
+    return $url;
+  }
+
+}
