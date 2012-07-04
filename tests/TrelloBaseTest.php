c1df5ea3f69c99d1387cd6b12c05dbd1a4fb49a3
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-07-04 11:27:19 -0500

    Added buildRequest tests

--- tests/TrelloBaseTest.php
+++ tests/TrelloBaseTest.php
@@ -11,12 +11,14 @@
  */
 class TrelloBaseTest extends PHPUnit_Framework_TestCase {
   protected $client;
+  protected $clientWrongKey;
 
   /**
    * Set up our basic tests
    */
   protected function setUp() {
     $this->client = new Trello('brianaltenhofelusertest', '246bd36112b51ad571f1455152bf7dc7');
+    $this->clientWrongKey = new Trello('brianaltenhofelusertest', 'xxxxd36112b51ad571f1455152bfxxxx');
   }
 
   public function testApiUrl() {
@@ -24,6 +26,31 @@ class TrelloBaseTest extends PHPUnit_Framework_TestCase {
     $result = $this->client->apiUrl('/members/' . $this->client->username, '');
 
     $this->assertEquals($expected, $result, 'Successfully build apiUrl ' . $expected);
+  }
+
+  public function testBuildRequest() {
+    // Test that we can see ourselves on Trello
+    $expected = 200;
+    $result = $this->client->buildRequest($this->client->apiUrl('/members/' . $this->client->username, ''));
+    $this->assertEquals($expected, $result->response->code, 'Successful request for own user data');
+
+    // Test that we can see other users on Trello
+    $expected = 200;
+    $result = $this->client->buildRequest($this->client->apiUrl('/members/brianaltenhofel', ''));
+    $this->assertEquals($expected, $result->response->code, 'Successful request for data about another user');
+
+    // Test that an invalid key will fail properly
+    $expected = 401;
+    $result = $this->clientWrongKey->buildRequest($this->client->apiUrl('/members/' . $this->client->username, ''));
+    $this->assertEquals($expected, $result->response->code, 'Invalid key fails properly');
+
+    // Test that an invalid username will fail properly
+    $expected = 404;
+    $result = $this->clientWrongKey->buildRequest($this->client->apiUrl('/members/admin', ''));
+    $this->assertEquals($expected, $result->response->code, 'Invalid username fails properly');
+
+
+
   }
 
 }
