20575333dab9e8ac5a43721e7ab2eb3370f302c5
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-07-04 15:57:58 -0500

    Updated listBoards test

--- tests/TrelloBaseTest.php
+++ tests/TrelloBaseTest.php
@@ -51,8 +51,7 @@ class TrelloBaseTest extends PHPUnit_Framework_TestCase {
 
   public function testListBoards() {
     // Test that we are using a proper endpoint and our key is valid
-    $expected = 200;
     $result = $this->client->listBoards($this->client->username);
-    $this->assertTrue($expected == $result->code, 'Improper endpoint or invalid key. See other tests to determine root cause.');
+    $this->assertTrue(!empty($result), 'Received an empty array');
   }
 }
