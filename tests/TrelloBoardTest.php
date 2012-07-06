878e4fa988856714cf0591b411472d7ad94cae07
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-07-06 00:04:10 -0500

    Fixed misplaced closing ) that resulted in parse error for Build 104

--- tests/TrelloBoardTest.php
+++ tests/TrelloBoardTest.php
@@ -17,7 +17,7 @@ class TrelloBoardTest extends TrelloBaseTest {
   public function testListBoards() {
     // Test that we are using a proper endpoint and our key is valid
     $result = $this->client->listBoards($this->client->username);
-    $this->assertTrue('200' === $result->code), 'Unsuccessful request for board listing');
+    $this->assertTrue('200' === $result->code, 'Unsuccessful request for board listing');
     $data = $this->decode($result->data);
     $this->assertTrue(!empty($data), 'Request for board listing returned an empty array');
   }
