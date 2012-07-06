40920d34fa770b3a7dbec1e04746b9d9a2d5b514
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-07-05 22:39:52 -0500

    Added framework to test controlled failures on boards

--- tests/TrelloBoardTest.php
+++ tests/TrelloBoardTest.php
@@ -20,4 +20,14 @@ class TrelloBoardTest extends TrelloBaseTest {
     $this->assertTrue(!empty($result), 'Received an empty array');
   }
 
+  /**
+   * A good test suite makes sure that we do fail where we are supposed to
+   * fail. This set of tests is to ensure that.
+   */
+  public function testBoardsControlledFailures() {
+    // Test that listing boards with a presumably invalid username works
+    $result= $this->client->listBoards('admin');
+    $this->assertTrue();
+  }
+
 }
